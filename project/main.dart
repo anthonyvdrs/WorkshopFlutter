55c56a45e79e795f8cb5dae99f561b6a84b34629
Menozzi <lorenzo.menozzi5@gmail.com>
2019-10-21 12:12:55 +0200

    translation

--- project/main.dart
+++ project/main.dart
@@ -63,20 +63,20 @@ class NewTodoDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: Text('New todo'),
+      title: Text('Nouvelle Tâche'),
       content: TextField(
         controller: controller,
         autofocus: true,
       ),
       actions: <Widget>[
         FlatButton(
-          child: Text('Cancel'),
+          child: Text('Annuler'),
           onPressed: () {
             Navigator.of(context).pop();
           },
         ),
         FlatButton(
-          child: Text('Add'),
+          child: Text('Ajouter'),
           onPressed: () {
             final todo = new Todo(title: controller.value.text);
             controller.clear();
